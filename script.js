97ee4a06595cc2f7afd10025e1408ed7275411f4
Sneha742000 <snehakumar758@gmail.com>
2025-06-01 14:21:40 +0530

    updated nostra project

---
+++ script.js
@@ -0,0 +1,9 @@
+    document.getElementById("close-banner").addEventListener("click", function () {
+        document.getElementById("offer-banner").style.display = "none";
+    });
+
+var new = document.getElementById("new-arrival")
+
+new.addEventListener("click", function(){
+    
+})
